97193589466123ab0666ea31a23198eb6d718d7f
dwjrgal <dwjrgl651@gmail.com>
2024-06-11 21:50:05 +0800

    first-commit

---
+++ main.js
@@ -0,0 +1,92 @@
+// Bodlogo-3
+
+let a=12, b=45, c=30;
+let z =45;
+
+if (a>b) {
+    console.log(a>b);
+
+}else if (a <b) {
+    console.log (a>b);
+
+}else if (b<c){
+    console.log(b<c);
+    
+}else if (b >c){
+    console.log (b>c);
+
+}else if (c <a){
+
+}else if (c >a){
+    
+}
+
+
+console.log("Hamgiin ih huvisagchtai too: ",z);
+
+
+// Bodlogo-5
+
+
+
+
+
+// Bodlogo-6
+
+let x =85, y =75, o=96, f=69; 
+console.log("80-s ih toonuudiin niilber:",x+o);
+
+// Bodlogo-7
+
+let q =3, w=7,e=2, r=4;
+
+console.log("5-с бага тоонуудын үржвэр:",q*e*r);
+
+
+// Bodlogo-8
+
+let scoreBat =67;
+let scoreOchir =59;
+let name = "Bat";
+let name2 = "Ochir"
+
+console.log(name,-scoreBat,"Hangalttai");
+
+if (scoreBat <60){
+    console.log("Mash muu:",scoreBat===60);
+}else if (scoreBat >=60){
+    console.log ("Hangalttai",);
+}
+
+
+console.log(name2,-scoreOchir,"Mash muu");
+
+if (scoreOchir <60){
+    console.log("Mash muu")
+}else if (scoreOchir >=60)
+    console.log("Hangalttai")
+
+
+    // Bodlogo-9
+
+    let r1 = 120;
+    let r2 = 33;
+    let r3 = 10;
+
+    console.log("Hamgiin baga huvisagch:",r3);
+
+
+    // Bodlogo-10
+
+    console.log("A team niit onoo:",96+108+89);
+
+    console.log(" B team niit onoo:",88+91+110);
+
+    console.log("2 bagiin dundaj onoo:",(293+289)/2);
+
+    if(293===289){
+        console.log("Tentssen");
+    }else if (293>289);{
+        console.log("A team turuulsen");
+    }
+
