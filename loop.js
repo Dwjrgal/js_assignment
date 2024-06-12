b738c640a54322b5824ed5ce18791771276d7f05
dwjrgal <dwjrgl651@gmail.com>
2024-06-12 13:01:35 +0800

    loop assignment

---
+++ loop.js
@@ -0,0 +1,38 @@
+// Bodlogo-1
+
+
+for (let i = 0; i <=10; i++){
+    console.log ("i = ", i);
+}
+
+
+// Bodlogo-2
+
+for (let i =0; i<=10; i++){
+    i+=i%2===0;
+    console.log ("Sondgoi toonuud:", i);
+}
+
+
+// Bodlogo-3
+
+for (let i =0; i<=10; i++){
+    i+=i%2===1;
+    console.log ("Tegsh toonuud:", i);
+}
+
+
+// Bodlogo-4
+
+let sum = 0;
+let too = 0;
+for (let tooluur = 1; tooluur <= 100; tooluur++) {
+  too++;
+  sum = sum + too;
+}
+console.log("SUM:", sum);
+
+
+// Bodlogo-5
+
+
--- main.js
+++ main.js
@@ -1,33 +1,49 @@
-// Bodlogo-3
+// / Bodlogo-1
 
-let a=12, b=45, c=30;
-let z =45;
+let myAge = 19;
 
-if (a>b) {
-    console.log(a>b);
+if (myAge < 25){
+    console.log("below 25");
+}else if (myAge > 25) {
+    console.log("above 25");
+}
 
-}else if (a <b) {
-    console.log (a>b);
+// Bodlogo-2
 
-}else if (b<c){
-    console.log(b<c);
-    
-}else if (b >c){
-    console.log (b>c);
+let n = 3
 
-}else if (c <a){
+if (n %2 ===0){
+    console.log("It's even number");
+}else {
+    console.log ("it's odd number");
 
-}else if (c >a){
-    
 }
 
 
-console.log("Hamgiin ih huvisagchtai too: ",z);
+// Bodlogo-3
+
+let a=12, b=45, c=30;
+
+if (a<b && b>c) {
+    console.log("Hamgiin ih huvisagchtai too: ",b);
+}else if(a>b && a>c){
+    console.log("Hamgiin ih huvisagchtai too: ",a);
+}else if (c >a && c >b) {
+    console.log("Hamgiin ih huvisagchtai too: ",c);
+}
 
 
-// Bodlogo-5
+// Bodlogo-4
 
+let b1 = 12, b2=45, b3 =30;
 
+if (b1<b2 && b1<b3){
+    console.log("Hamgiin baga huvisagchtai too: ",b1);
+}else if (b2<b1 && b2<b3){
+    console.log("Hamgiin  baga huvisagchtai too: ",b2);
+}else if (b3<b1 && b3<b2){
+    console.log("Hamgiin baga huvisagchtai too: ",b3);
+}
 
 
 
@@ -35,13 +51,33 @@ console.log("Hamgiin ih huvisagchtai too: ",z);
 // Bodlogo-6
 
 let x =85, y =75, o=96, f=69; 
-console.log("80-s ih toonuudiin niilber:",x+o);
+
+if (x>80 && o >80){
+    console.log("80-s ih toonuudiin niilber:",x+o);
+}else if (y>80 && f>80){
+    console.log("80-s ih toonuudiin niilber:",y+o);
+}else if (o>80 && f>80){
+    console.log("80-s ih toonuudiin niilber:",o+x);
+}else if (f >80 && x>80){
+    console.log("80-s ih toonuudiin niilber:",f+o);
+}
+
 
 // Bodlogo-7
 
 let q =3, w=7,e=2, r=4;
 
-console.log("5-с бага тоонуудын үржвэр:",q*e*r);
+
+if (q<5 && e<5 && r<5){
+    console.log("5-с бага тоонуудын үржвэр:",q*e*r);
+}else if (r<5 && w<5 && e<5){
+    console.log("5-с бага тоонуудын үржвэр:",w*e*r);
+}else if (q<5 && w<5 && e<5){
+    console.log("5-с бага тоонуудын үржвэр:",q*w*e);
+
+}
+
+
 
 
 // Bodlogo-8
@@ -55,7 +91,9 @@ console.log(name,-scoreBat,"Hangalttai");
 
 if (scoreBat <60){
     console.log("Mash muu:",scoreBat===60);
-}else if (scoreBat >=60){
+}else if (scoreBat >=60 && scoreBat < 70){
+    console.log ("Hangalttai",);
+}else if (scoreBat >=70 && scoreBat < 80){
     console.log ("Hangalttai",);
 }
 
@@ -74,7 +112,14 @@ if (scoreOchir <60){
     let r2 = 33;
     let r3 = 10;
 
-    console.log("Hamgiin baga huvisagch:",r3);
+    if ( r1<r2 && r1<r3){
+        console.log("Hamgiin baga huvisagch:",r1);
+    }else if (r2<r1 && r2<r3){
+        console.log("Hamgiin baga huvisagch:",r2);
+    }else if (r3<r1 && r3<r2){
+        console.log("Hamgiin baga huvisagch:",r3);
+    }
+
 
 
     // Bodlogo-10
@@ -106,6 +151,8 @@ if (scoreOchir <60){
 
     if(age >=10){   
     console.log("Dugui unaj bolno",age >=10)
+    }else if (age <10){
+        console.log ("Not allowed to ride");
     }
 
 
@@ -114,6 +161,17 @@ if (scoreOchir <60){
 
     let t1 =7,t2=22, t3=13, t4=30;
 
-    console.log ("11-t huvaagdahgui toonuudin niilber:",t1+t3+t4);
+    if (t1 %11===1){
+        console.log ("11-t huvaagdahgui toonuudin niilber:",t1+t3+t4); 
+    }else if (t2%11===1){
+        console.log ("11-t huvaagdahgui toonuudin niilber:",t2+t3+t4);
+    }else if (t3%11===1){
+        console.log ("11-t huvaagdahgui toonuudin niilber:",t1+t3+t4);
+
+    }else if (t4%11===1){
+        console.log ("11-t huvaagdahgui toonuudin niilber:",t1+t3+t4);
+
+    }
+
 
 
