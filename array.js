b4bc43a34573765ed8b4718466e60a116bea2967
dwjrgal <dwjrgl651@gmail.com>
2024-06-13 12:18:26 +0800

    array -assignment

---
+++ array.js
@@ -0,0 +1,23 @@
+// Js -ARRAY EXERCISE 1
+
+// Daalgavar-1 
+
+let fruits = ["apple", "banana", "orange", "kiwi", "grape",]
+
+ console.log ("Ehnii element:",fruits[0]);
+ console.log ("Suuliin element:",fruits[4]);
+ console.log ("3-r element:",fruits[2]);
+
+
+
+// Daalgavar-2
+
+let arrayLength = 9;
+
+let array =[5, 6, 12, 19, 121, 1, 7, 9,63,]
+
+for (let i=0; i <=arrayLength; i++){
+    if (i%2 !==0){
+        console.log ("Sondgoi toonuud:",i);
+    }
+}
