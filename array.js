e244a91e8056b98b33c97902749d9666b989bde5
dwjrgal <dwjrgl651@gmail.com>
2024-06-13 14:01:34 +0800

    array bodlogo-3

--- array.js
+++ array.js
@@ -12,12 +12,69 @@ let fruits = ["apple", "banana", "orange", "kiwi", "grape",]
 
 // Daalgavar-2
 
-let arrayLength = 9;
 
 let array =[5, 6, 12, 19, 121, 1, 7, 9,63,]
 
-for (let i=0; i <=arrayLength; i++){
-    if (i%2 !==0){
-        console.log ("Sondgoi toonuud:",i);
+for (let i=0; i <=array.length; i++){
+    if (array [i]%2 ===1){
+        console.log ("Sondgoi toonuud:",array[i]);
+    }else {
+        console.log (" Tegsh toonuud:", array[i]);
+    }
+}
+
+
+
+// DAALGAVAR-3
+
+let arrayOfNumbers = [43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11]
+
+// daalgavar-3/1 array lenght oloh:
+
+console.log ( "Array length:",arrayOfNumbers.length);
+
+// daalgavar-3/2 array dotorh toonuudiin niilber:
+
+let sum =0;
+let i =0;
+
+for (let  i=0; i<= array.length; i++ ){
+    i++;
+    sum=sum +arrayOfNumbers[i];
+}
+
+console.log ("SUM:",sum);
+
+
+// daalgavar-3/3 array dotorh toonuudiin average :
+
+console.log ("Average :",sum/arrayOfNumbers.length);
+
+// daalgavar-3/4 array dotorh  sondgoi toonuudiin niilber  :
+
+let numb =0;
+let som =0;
+
+for (let i=0; i <=arrayOfNumbers.length; i++){
+    if (arrayOfNumbers [i]%2 ===1){
+        som++;
+        numb = numb +arrayOfNumbers[i];
+        console.log ("Sondgoi toonuudiin niilber:",numb);
+
+    }
+}
+
+
+// daalgavar-3/5 array dotorh  tegsh toonuudiin niilber  :
+
+
+let nom =0;
+let sem =0;
+
+for (let i=0; i <=arrayOfNumbers.length; i++){
+    if (arrayOfNumbers [i]%2 ===0){
+        nom = nom +arrayOfNumbers[i];
+        console.log ("Sondgoi toonuudiin niilber:",n);
+
     }
 }
