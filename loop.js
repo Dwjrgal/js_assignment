0c38be912d5ce2554bf141798aff783435e2a5d2
dwjrgal <dwjrgl651@gmail.com>
2024-06-13 11:39:06 +0800

    8 9r bodlogo

--- loop.js
+++ loop.js
@@ -9,12 +9,14 @@ for (let i = 0; i <=10; i++){
 // Bodlogo-2
 
 for (let i =0; i<=10; i++){
-    i+=i%2===0;
-    console.log ("Sondgoi toonuud:", i);
+    if(i%2!==0){
+        console.log ("Sondgoi toonuud:", i);
+    }
+    
 }
 
 
-// Bodlogo-3
+// // Bodlogo-3
 
 for (let i =0; i<=10; i++){
     i+=i%2===1;
@@ -22,7 +24,7 @@ for (let i =0; i<=10; i++){
 }
 
 
-// Bodlogo-4
+// // Bodlogo-4
 
 let sum = 0;
 let too = 0;
@@ -33,7 +35,7 @@ for (let tooluur = 1; tooluur <= 100; tooluur++) {
 console.log("SUM:", sum);
 
 
-// Bodlogo-5
+// // Bodlogo-5
 
 let bum = 0;
 let mum =0; 
@@ -45,7 +47,7 @@ for (let tooluur = 1; tooluur <= 101; tooluur++) {
   console.log("SUM:", bum);
   
 
-// Bodlogo-6
+// // Bodlogo-6
 
 let number= 4;
 let factorial = 1;
@@ -57,14 +59,22 @@ console.log ("4-iin factorial:",factorial);
 
 
 
-// Bodlogo-7
+// // Bodlogo-7
 
-let a=1, b= 2, c=3;
+let numb = 123; 
+let digit = 0;
+let som =0;
 
-console.log ("Цифрүүдийн нийлбэр:",a+b+c);
+while(numb!== 0){
+    digit = numb %10;
+    numb =numb -digit;
+    numb =numb /10;
+    som =som +digit;
+}
+console.log ("Digit som:",som);
 
 
-// Bodlogo-8
+// // Bodlogo-8
 
 let x1 =1, x2=2, x3=3, x4 =4;
 
@@ -91,17 +101,28 @@ for (let n=0; n<=8; n++){
     console.log ("Sondgoi toonuud:", n);
 }
 
-console.log ("Sondgoi toonuudiin niilber:",n);
+console.log ("Sondgoi tsipriin niilber:",sem);
 
 
-// Bodlogo-10
-
-for (let too =1; too <=10; too++){
-    too+=too%2 ===1;
-    console.log ("Tegsh toonuud:",too);
-}
 
 
+// // Bodlogo-10
+
+// let odd = 0;
+// let des = 0;
+// let sim = 0;
+
+// while (odd <=20){
+//     des = odd % 10;
+//     odd =odd - des;
+//    odd = odd / 10;
+//     if(des % 2 === 0) {
+//         console.log("tegsh", des);
+//         sim = sim + des;
+//     }
+// }
+
+// console.log ("N hurtelh toonii tegsh tsipruudiin niilber:", sim);
 
 
 // Bodlogo-11
