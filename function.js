365c01afabbf44d72fecfc371aa4796af628bc01
dwjrgal <dwjrgl651@gmail.com>
2024-06-17 11:06:17 +0800

    js function

---
+++ function.js
@@ -0,0 +1,52 @@
+let a = 10;
+let b =20;
+
+
+function hevle (){
+    console.log ("2 toonii niilber:", a+b);
+
+}
+
+
+function hevle2(too1, too2){
+    console.log ("2 toonii niilber:", too1+too2);
+}
+
+
+function nemeh (x, y ){
+    return x+y;
+}
+
+
+hevle();
+hevle2(11,33);
+
+
+let add = nemeh (11, 22);
+console.log ("Nemeh:",add);
+
+
+
+function hasah (z , m){
+  return z-m;
+}
+
+   let sub = hasah (8 , 2);
+   console.log ("Hasah:",sub);
+
+
+
+function urjikh (l, p  ){
+    return l*p;
+}
+
+  let mul =urjikh ( 8 ,9);
+  console.log ("Urjikh:", mul);
+
+
+  function huvaah ( n, c){
+    return n/c;
+  }
+
+  let div = huvaah (16 ,8);
+  console.log ("Huvaah:",div);
