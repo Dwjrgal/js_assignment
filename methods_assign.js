13ec77d2a6409ef5188913d4247d3c974ecc1060
dwjrgal <dwjrgl651@gmail.com>
2024-06-19 23:54:51 +0800

    aaaeeeii

--- methods_assign.js
+++ methods_assign.js
@@ -20,22 +20,28 @@ console.log ("Array uusgeh:", empty)
 
 
 
-// Daalgavar -3 
+// Daalgavar -3 garaas oruulsan toonii sum avg oldog fuction bichih
 
     
 let arr = prompt("Ta ymar negen too oruulna uu:")
 
-function sumAvg ( arr ){
+function sumAvg ( random ){
 let sum = 0;
 let avg = 0;
 
-    for (let i =0; i <arr.lenght; i++){
+    for (let i =0; i <random.lenght; i++){
         sum +=arr[i];
     }
-    avg = sum / arr.lenght;
+    avg = sum / random.lenght;
     return avg;
 
 }
 
 let ar = sumAvg(1,2, 3,);
 console.log ("Hi", ar) 
+
+// ---- Daalagvar- 4 --- 
+// Ogogdson string iin buh usgiig tom bolgoh 
+
+let string = ['the quick brown fox'];
+
